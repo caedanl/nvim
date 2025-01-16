6530cdfa66a13a3f931784153815cf6a89f3412f
Caedan <caedanlavender@gmail.com>
2025-01-17 09:02:08 +1300

    chore: remove comments

--- lua/caedan/plugins/conform.lua
+++ lua/caedan/plugins/conform.lua
@@ -6,11 +6,8 @@ return {
 				formatters_by_ft = {
 					lua = { "stylua" },
 					terraform = { "terraform_fmt" },
-					-- Conform will run multiple formatters sequentially
 					python = { "isort", "black" },
-					-- You can customize some of the format options for the filetype (:help conform.format)
 					rust = { "rustfmt", lsp_format = "fallback" },
-					-- Conform will run the first available formatter
 					javascript = { "prettierd", "prettier", stop_after_first = true },
 					typescript = { "prettierd", "prettier", stop_after_first = true },
 					typescriptreact = { "prettierd", "prettier", stop_after_first = true },
